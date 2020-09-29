79be653194f9263131446a52cdf5f43f0c891dcf
ykrank <ykrank@gmail.com>
2020-09-29 16:59:37 +0800

    使用fastjson

--- plugin_base/src/main/java/uni/ykrank/plugin_base/utils/JsonUtil.java
+++ plugin_base/src/main/java/uni/ykrank/plugin_base/utils/JsonUtil.java
@@ -1,20 +1,18 @@
 package uni.ykrank.plugin_base.utils;
 
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 
 public class JsonUtil {
-    static ObjectMapper mapper = JsonKotlinUtil.INSTANCE.getMapper();
-
     @Nullable
     public static <T> T readValue(String src, Class<T> valueType) {
         try {
-            return mapper.readValue(src, valueType);
-        } catch (JsonProcessingException e) {
+            return JSON.parseObject(src, valueType);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
@@ -23,10 +21,18 @@ public class JsonUtil {
     @Nullable
     public static <T> T readValue(String src, TypeReference<T> typeReference) {
         try {
-            return mapper.readValue(src, typeReference);
-        } catch (JsonProcessingException e) {
+            return JSON.parseObject(src, typeReference);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    @NonNull
+    public static String toJSONString(@Nullable Object obj) {
+        if (obj == null) {
+            return "";
+        }
+        return JSON.toJSONString(obj);
     }
 }
